c05114ba0cf256e311b4e4f05717a21f8acf920f
SQ9MDD <rysieklabus@gmail.com>
2016-01-30 17:55:52 +0100

    automatyka podajnika
    Jesli temp nie wzrasta trzykrotne podanie paliwa co 90sec

--- CARBONEC.ino
+++ CARBONEC.ino
@@ -1,5 +1,7 @@
 /*  CARBONEC sterownik pieca na ekogroszek
- *  Copyright SQ9MDD Rysiek Labus
+ *  SQ9MDD Rysiek Labus @ 2016
+ *  
+ *  Software na licencji GPL v.2.0
  * 
  *  Założenia hardware:
  *    - sterowanie wentylatorem nadmuchu
@@ -39,10 +41,29 @@ static int praca_stop = A1;                             // LOW - praca, przycisk
 int flaga_rozruch = 0;                                  // flaga sygnalizacyjna rozruch pieca
 int flaga_awaria = 0;                                   // flaga sygnalizacyjna awarii pieca
 int pozwolenie_pracy_piec = 0;                          // flaga zezwolenia pracy pieca, ustawiana recznie lub z sieci
+int pozwolenie_pracy_went = 0;                          //
 int pozwolenie_pracy_podajnik = 0;                      // 
 int flaga_chwilowa_blokada_podajnika = 0;               // blokada by podajnik zbyt często nie podawał paliwa
+int licznik_podan_kolejnych = 0;
 unsigned long czas_wylaczyc_podajnik = 0;               //
 unsigned long czas_na_pomiar = 0;                       //
+unsigned long czas_resetu_podanie_kolejne = 0;
+
+// mierzymy temperature co 5 sekund
+void pomiar_temp(){
+  if(millis() >= czas_na_pomiar){
+    //tutaj robimy pomiar temperatury dallas
+
+    //zdejmowanie flagi rozruchu po osiągnięciu temperatury zadanej pieca
+    if(temperatura_pieca_odczyt >= temperatura_setpoint_pieca){
+      flaga_rozruch = 0;
+      pozwolenie_pracy_went = 0; //przeniesc do automat_wentylator
+    }
+    //ustawiamy czas nastepnego pomiaru
+    czas_na_pomiar = millis() + 5000;
+  }
+}
+
 
 //blokady i awarie
 void blokady(){
@@ -69,23 +90,38 @@ void sterowanie_pompa(){
 
 //funkcje sterowania wentylatorem
 void sterowanie_wentylatorem(){
-
+  if(flaga_rozruch == 1 || pozwolenie_pracy_went == 1){
+    digitalWrite(drv_went,LOW);
+  }else{
+    digitalWrite(drv_went,HIGH);
+  }
 }
 
 //funkcje sterowania podajnikiem
 void automat_podajnik(){
   //jesli jest pozwolenie pracy pieca i nie jest to rozruch i niema chwilowej blokady podajnika to
-  //podaj paliwo i zablokuj sie do osiagniecia temperatury 
+  //podaj paliwo i zablokuj sie do osiagniecia temperatury lub podaj za 90 sec (trzykrotnie)
   if(flaga_rozruch == 0 && pozwolenie_pracy_piec == 1 && flaga_chwilowa_blokada_podajnika == 0){
     if(temperatura_pieca_odczyt <= temperatura_wlacz_podajnik){
       uruchom_podajnik();
       flaga_chwilowa_blokada_podajnika = 1; 
+      czas_resetu_podanie_kolejne = millis() + 90000; 
     }
   }
+  //trzykrotny reset flagi blokady co 90 sekund jesli temperatura nie wzrasta 
+  if(temperatura_pieca_odczyt < temperatura_setpoint_pieca && licznik_podan_kolejnych < 3 && flaga_rozruch == 0 && pozwolenie_pracy_piec == 1){
+    if(millis() >= czas_resetu_podanie_kolejne){
+      flaga_chwilowa_blokada_podajnika = 0;
+      licznik_podan_kolejnych++; 
+      czas_resetu_podanie_kolejne = millis() + 90000;     
+    }    
+  }  
   //reset flagi podajnika jesli osiagnelismy temperature
+  //oraz jednoczesnie reset licznika podan kolejnych paliwa
   if(temperatura_pieca_odczyt >= temperatura_setpoint_pieca){
     flaga_chwilowa_blokada_podajnika = 0;
-  }
+    licznik_podan_kolejnych = 0;
+  } 
 }
 
 //uruchamianie podajnika sygnal reczny lub z automatyki
@@ -105,15 +141,6 @@ void sterowanie_podajnik(){
   }
 }
 
-void pomiar_temp(){
-  if(millis() >= czas_na_pomiar){
-    //tutaj robimy pomiar temperatury dallas
-
-    //ustawiamy czas nastepnego pomiaru
-    czas_na_pomiar = millis() + 5000;
-  }
-}
-
 //uruchamianie sterownika
 void setup() {
   pinMode(drv_ptt,OUTPUT);
@@ -121,6 +148,7 @@ void setup() {
   pinMode(drv_podajnik,OUTPUT);
   pinMode(drv_pompa_wody,OUTPUT);
   pinMode(reczny_podajnik,INPUT_PULLUP);
+  pinMode(praca_stop,INPUT_PULLUP);
   digitalWrite(drv_ptt,LOW);
   digitalWrite(drv_went,HIGH);
   digitalWrite(drv_podajnik,HIGH);
@@ -130,17 +158,28 @@ void setup() {
 
 //petla glowna
 void loop() {
+  
   //klawisz sterowania recznego podajnikiem
   if(digitalRead(reczny_podajnik) == LOW){
     delay(100);
     if(digitalRead(reczny_podajnik) == LOW){
       uruchom_podajnik();
-      
     }
   }
+  //klawisz uruchamiania pieca
+  if(digitalRead(praca_stop) == LOW){
+    delay(100);
+    if(digitalRead(praca_stop) == LOW){
+      pozwolenie_pracy_piec = 1;
+      flaga_rozruch = 1;
+      flaga_awaria = 0;
+    }
+  }  
 
   //funkcje uruchamiane zawsze
-  sterowanie_podajnik();
   pomiar_temp();
+  sterowanie_wentylatorem();
+  sterowanie_pompa();
   automat_podajnik();
+  sterowanie_podajnik();
 }
