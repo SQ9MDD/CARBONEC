0cf6ba248553f73a225e667dbb854562197ca202
SQ9MDD <rysieklabus@gmail.com>
2016-01-30 16:54:37 +0100

    automatyka podajnika v.1.0
    wstepny algorytm podajnika

--- CARBONEC.ino
+++ CARBONEC.ino
@@ -15,29 +15,60 @@
 //
 
 //konfiguracja (jeśli nie uzywasz domoticza do konfiguracji)
-unsigned long podajnik_praca_czas = 15000; //wstepnie dosypujemy porcco 10minut 
+unsigned long podajnik_praca_czas = 15000;              // czas jednorazowego podawania wegla
+int temperatura_pieca_odczyt = 0;
+int temperatura_alarm_pieca = 750;                      // C*10 temperatura przy ktorej uruchamiam alarm i powiadomienie
+int temperatura_setpoint_pieca = 550;                   // C*10 temperatura nastawa pieca (wylaczenie nadmuchu, wyłaczenie flagi rozruch)
+int temperatura_wlacz_nadmuch = 530;                    // C*10 temperatura wlaczenia nadmuchu
+int temperatura_wlacz_podajnik = 520;                   // C*10 temperatura wlaczenia podajnika
 
 //wejscia wyjscia
 static int drv_ptt = 2;
 static int drv_went = 3;
 static int drv_podajnik = 4;                            // LOW - praca, HIGH - stop
 static int drv_pompa_wody = 5;
-static int sens_woda_temp = 0;
+static int sens_woda_temp = 6;
 static int sens_paliwo_poziom = 0;
-static int reczny_podajnik = A0;
+static int reczny_podajnik = A0;                        //LOW - podawanie ręczne
+static int praca_stop = A1;                             //LOW - praca, przycisk chwilowy
 
 
 //zmienne pomocnicze
-int pozwolenie_pracy_piec = 0;
+int flaga_rozruch = 0;
+int pozwolenie_pracy_piec = 0;                          //flaga zezwolenia pracy pieca, ustawiana recznie lub z sieci
 int pozwolenie_pracy_podajnik = 0;
+int flaga_chwilowa_blokada_podajnika = 0;
 unsigned long czas_wylaczyc_podajnik = 0;
+unsigned long czas_na_pomiar = 0;
+
+//funkcje sterowania wentylatorem
+void sterowanie_wentylatorem(){
+
+}
 
 //funkcje sterowania podajnikiem
+void automat_podajnik(){
+  //jesli jest pozwolenie pracy pieca i nie jest to rozruch i niema chwilowej blokady podajnika to
+  //podaj paliwo i zablokuj sie do osiagniecia temperatury 
+  if(flaga_rozruch == 0 && pozwolenie_pracy_piec == 1 && flaga_chwilowa_blokada_podajnika == 0){
+    if(temperatura_pieca_odczyt <= temperatura_wlacz_podajnik){
+      uruchom_podajnik();
+      flaga_chwilowa_blokada_podajnika = 1; 
+    }
+  }
+  //reset flagi podajnika jesli osiagnelismy temperature
+  if(temperatura_pieca_odczyt >= temperatura_setpoint_pieca){
+    flaga_chwilowa_blokada_podajnika = 0;
+  }
+}
+
+//uruchamianie podajnika sygnal reczny lub z automatyki
 void uruchom_podajnik(){
   pozwolenie_pracy_podajnik = 1;
   czas_wylaczyc_podajnik = millis() + 15000;
 }
 
+//wyłączanie podajnika
 void sterowanie_podajnik(){
   if(pozwolenie_pracy_podajnik == 1){
     digitalWrite(drv_podajnik,LOW);
@@ -48,7 +79,16 @@ void sterowanie_podajnik(){
   }
 }
 
+void pomiar_temp(){
+  if(millis() >= czas_na_pomiar){
+    //tutaj robimy pomiar temperatury dallas
 
+    //ustawiamy czas nastepnego pomiaru
+    czas_na_pomiar = millis() + 5000;
+  }
+}
+
+//uruchamianie sterownika
 void setup() {
   pinMode(drv_ptt,OUTPUT);
   pinMode(drv_went,OUTPUT);
@@ -62,15 +102,19 @@ void setup() {
   Serial.begin(9600);
 }
 
+//petla glowna
 void loop() {
   //klawisz sterowania recznego podajnikiem
   if(digitalRead(reczny_podajnik) == LOW){
     delay(100);
     if(digitalRead(reczny_podajnik) == LOW){
       uruchom_podajnik();
+      
     }
   }
 
   //funkcje uruchamiane zawsze
   sterowanie_podajnik();
+  pomiar_temp();
+  automat_podajnik();
 }
