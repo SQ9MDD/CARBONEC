7afeb4612aae3aaebd9887b6a2cb81e1ff29e6e7
SQ9MDD <rysieklabus@gmail.com>
2016-01-30 15:32:31 +0100

    First sample

---
+++ CARBONEC.ino
@@ -0,0 +1,19 @@
+static int drv_went = 0;
+static int drv_podajnik = 0;
+static int drv_pompa_wody = 0;
+static int sens_woda_temp = 0;
+static int sens_paliwo_poziom = 0;
+
+void sterowanie_podajnik(){
+
+}
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
